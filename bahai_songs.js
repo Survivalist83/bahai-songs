a995b7e382521511ad3bbbd8be4c4420d072b841
Survivalist83 <benmaxtennant@gmail.com>
2026-01-22 14:04:48 -0800

    initial commit

---
+++ bahai_songs.js
@@ -0,0 +1,165 @@
+let songList = []
+
+function pageLoad() {
+    document.getElementById("hideMe").style.display = "none";
+
+    // This loads (but hides) the songs, only showing the requested one.
+    let currentSong = getCurrentSong();
+    for (let i = 0; i < BAHAI_SONGS_DATA.length; i++) {
+        songList.push(BAHAI_SONGS_DATA[i].meta.name);
+        loadSong(i, currentSong);
+    }
+
+    // This adds buttons to select a song.
+    const mainMenu = document.getElementById("mainMenu");
+    for (let i = 0; i < songList.length; i++) {
+        const mainMenuBtn = document.createElement("button");
+        mainMenuBtn.addEventListener("click", () => {showSong(i, true);});
+        mainMenuBtn.innerText = songList[i];
+        mainMenu.appendChild(mainMenuBtn);
+    }
+
+    // This handles users clicking the back button.
+    window.addEventListener("popstate", () => {
+        currentSong = getCurrentSong();
+        let currentSongIndex = songList.indexOf(currentSong);
+        showSong(currentSongIndex, false);
+    })
+        
+};
+
+// Initializes the website on page load such that every song is loaded, but hidden.
+function loadSong(songNumber, currentSong) {
+    const contentDiv = document.getElementById("contentDiv")
+
+    const lyrics = BAHAI_SONGS_DATA[songNumber].lyrics;
+    const meta = BAHAI_SONGS_DATA[songNumber].meta;
+    const outerDiv = document.createElement("div");
+    outerDiv.id = "songOuterDiv" + songNumber;
+    outerDiv.classList.add("songOuterDiv");
+    
+    // Hides the song by default, unless the URL says this is the one to be displayed.
+    if (songList[songNumber] !== currentSong) {
+        outerDiv.style.display = "none";
+    }
+
+    // Song header.
+    const songTitle = document.createElement("h1");
+    songTitle.innerText = meta.name;
+    outerDiv.appendChild(songTitle);
+
+    // Song source.
+    const songLink = document.createElement("a");
+    songLink.href = meta.sourceLink;
+    songLink.target = "_blank";
+    songLink.innerHTML = meta.sourceName;
+    outerDiv.appendChild(songLink);
+
+    // Adds a blank space after song source
+    // const songLinkSpace = document.createElement("p");
+    // songLinkSpace.innerHTML = "&nbsp;";
+    // outerDiv.appendChild(songLinkSpace);
+
+    // Checks if the song has any call and response.
+    let hasCallAndResponse = false;
+    for (let i = 0; i < lyrics.length; i++) {
+        if (lyrics[i].sectionMeta && lyrics[i].sectionMeta.callAndResponse) {
+            hasCallAndResponse = true;
+            break;
+        }
+    }
+
+    // Checks which columns the song is using (default to only column 0).
+    let columnList = [];
+    for (let i = 0; i < lyrics.length; i++) {
+        columnList.push(lyrics[i].sectionMeta?.column ?? 0)
+    }
+    columnList = [...new Set(columnList)];
+    
+    // Makes a table (only one row) to use for columns. Every song is in the table, even if it only has one column.
+    const table = document.createElement("table");
+    const tableRow = document.createElement("tr");
+    table.appendChild(tableRow);
+    for (let i = 0; i < columnList.length; i++) {
+        const tableColumn = document.createElement("td");
+        tableColumn.id = "songTdColumn" + columnList[i];
+        tableColumn.classList.add("songTdColumn");
+        tableRow.appendChild(tableColumn);
+    }
+
+    for (let i = 0; i < lyrics.length; i++) {
+        const sectionLyrics = lyrics[i].sectionLyrics;
+        const sectionMeta = lyrics[i].sectionMeta;
+        const sectionDiv = document.createElement("div");
+        
+        // Adds "Call and response:" or "All together:" to each section, if needed.
+        if (sectionMeta && sectionMeta.callAndResponse) {
+            const callAndResponse = document.createElement("p");
+            callAndResponse.innerText = "Call and response:";
+            callAndResponse.classList.add("songLyric", "bold");
+            sectionDiv.appendChild(callAndResponse);
+        } else if (hasCallAndResponse) {
+            const allTogether = document.createElement("p");
+            allTogether.innerText = "All together:";
+            allTogether.classList.add("songLyric", "bold");
+            sectionDiv.appendChild(allTogether);
+        }
+
+        // Adds the verses themselves.
+        for (let j = 0; j < sectionLyrics.length; j++) {
+            const lyric = document.createElement("p");
+            lyric.innerHTML = sectionLyrics[j];
+            lyric.classList.add("songLyric");
+            sectionDiv.appendChild(lyric);
+        }
+
+        // Adds repetitions - i.e. (×2)
+        if (sectionMeta && sectionMeta.repetitions) {
+            const repetitions = document.createElement("p");
+            repetitions.innerText = "(×" + sectionMeta.repetitions + ")";
+            repetitions.classList.add("songLyric");
+            sectionDiv.appendChild(repetitions);
+        }
+
+        // Adds a space between sections (if this isn't the last section)
+        if (Number(i) + 1 !== lyrics.length && !(sectionMeta && sectionMeta.repetitions) && !hasCallAndResponse) {
+            const sectionSpacing = document.createElement("p");
+            sectionSpacing.innerHTML = "&nbsp;";
+            sectionDiv.appendChild(sectionSpacing);
+        }
+
+        const column = sectionMeta?.column ?? 0
+        tableRow.querySelector("#songTdColumn" + column).appendChild(sectionDiv);
+    }
+
+    outerDiv.appendChild(table);
+    contentDiv.appendChild(outerDiv);
+}
+
+// A helper function for function loadSong() that creates a blank div for visual appeal/spacing.
+function createBlankDiv() {
+
+}
+
+// Returns the index of the song the URL says we should display. Returns -1 on errors.
+function getCurrentSong() {
+    const params = new URLSearchParams(window.location.search);
+    const currentSong = params.get("s");
+    return currentSong;
+}
+
+// Shows one specific song.
+function showSong(songNumber, boolChangeHistory) {
+    document.querySelectorAll(".songOuterDiv").forEach(songOuterDiv => {songOuterDiv.style.display = "none";});
+
+    if (songNumber != -1) {
+        document.getElementById("songOuterDiv" + songNumber).style.display = "flex";
+    }
+
+    // Changes the "s" query string parameter to the current song's name so that the URL is easily shareable with others.
+    if (boolChangeHistory) {
+        const params = new URLSearchParams(window.location.search);
+        params.set("s", songList[songNumber]);
+        window.history.pushState({}, "", window.location.pathname + "?" + params.toString());
+    }
+}
---
+++ bahai_songs_data.js
@@ -0,0 +1,162 @@
+const BAHAI_SONGS_DATA = [
+    // {
+    //     "meta": {
+    //         "name": "",
+    //         "sourceName": "",
+    //         "sourceLink": "",
+    //         "pageBreak": []
+    //     },
+    //     "lyrics": [
+    //         "",
+    //     ]
+    // },
+    {
+        "meta": {
+            "name": "Kindle the fire of love",
+            "sourceName": "Bahá'u'lláh, <i>The Seven Valleys</i>",
+            "sourceLink": "https://www.bahai.org/library/authoritative-texts/bahaullah/call-divine-beloved/4#639859466:~:text=Kindle%20the%20fire%20of%20love,the%20land%20of%20the%20lovers",
+        },
+        "lyrics": [
+            {
+                "sectionLyrics": [
+                    "Kindle the fire of love",
+                    "and burn away all things;",
+                    "Then set thy foot",
+                    "into the land of the lovers.",
+                ]
+            }
+        ]
+    },
+    {
+        "meta": {
+            "name": "O let the flame",
+            "sourceName": "<i>Gleanings from the Writings of Bahá'u'lláh</i>, CLIII",
+            "sourceLink": "https://www.bahai.org/r/413529355",
+            "pageBreak": []
+        },
+        "lyrics": [
+            {
+                "sectionMeta":{
+                    "callAndResponse": true,
+                    "repetitions": 2
+                },
+                "sectionLyrics":[
+                    "O let the flame",
+                    "of the love of God",
+                    "burn brightly within",
+                    "your radiant hearts.",
+                ]
+            },
+            {
+                "sectionMeta":{
+                    "repetitions": 2
+                },
+                "sectionLyrics":[
+                    "Feed it with the oil of",
+                    "Divine guidance, and",
+                    "protect it within the shelter",
+                    "of your constancy.",
+                ]
+            }
+        ]
+    },
+    {
+        "meta": {
+            "name": "May this boundless love",
+            "sourceName": "'Abdu'l-Bahá, Paris Talks",
+            "sourceLink": "https://www.bahai.org/r/640151780"
+        },
+        "lyrics": [
+            {
+                "sectionMeta": {
+                    "repetitions": 2
+                },
+                "sectionLyrics": [
+                    "May this boundless love",
+                    "so fill your hearts and minds",
+                    "that sadness may find",
+                    "no room to enter,",
+                    "and may you with joyful hearts",
+                    "soar",
+                    "like birds",
+                    "into Divine Radiance."
+                ]
+            }
+                    
+        ]
+    },
+    {
+        "meta": {
+            "name": "First of all",
+            "sourceName": "<i>'Abdu'l-Bahá on Divine Philosophy</i>, p. 112",
+            "sourceLink": "https://bahai-library.com/abdul-baha_divine_philosophy&chapter=all#112"
+        },
+        "lyrics": [
+            {
+                "sectionLyrics": [
+                    "(First of all,)",
+                    "be ready to sacrifice your lives",
+                    "for one another,",
+                    "to prefer the general well-being",
+                    "to your personal well-being.",
+                    "Create relationships that nothing can shake;",
+                    "form an assembly that nothing can break up,",
+                    "have a mind that",
+                    "never ceases (x2)",
+                    "acquiring riches",
+                    "nothing can destroy",
+                    "that nothing can destroy. (x3)"
+                ]
+            },
+            {
+                "sectionMeta": {
+                    "repetitions": 2,
+                    "column": 1
+                },
+                "sectionLyrics": [
+                    "If love did not exist,",
+                    "what of reality would remain?"
+                ]
+            },
+            {
+                "sectionMeta": {
+                    "column": 1
+                },
+                "sectionLyrics": [
+                    "It is the fire of the love of God",
+                    "which renders man superior",
+                    "to the animal. (x4)"
+                ]
+            },
+            {
+                "sectionMeta": {
+                    "repetitions": 2,
+                    "column": 1
+                },
+                "sectionLyrics": [
+                    "Strengthen this superior force",
+                    "through which is attained",
+                    "all the progress in the world"
+                ]
+            },
+            {
+                "sectionMeta": {
+                    "repetitions": 3,
+                    "column": 1
+                },
+                "sectionLyrics": [
+                    "<i>Boys:</i> in the world.",
+                    "<i>Girls:</i> in the world."
+                ]
+            },
+            {
+                "sectionMeta": {
+                    "column": 1
+                },
+                "sectionLyrics": [
+                    "all the progress in the world."
+                ]
+            }
+        ]
+    }
+]
